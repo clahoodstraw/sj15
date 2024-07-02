4e734ef992762d69d5cb10d16590c26b32ed3731
clahoodstraw <jojowatchout@gmail.com>
2024-07-02 21:58:54 +0900

    new

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path');
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  //res.send('Hello sejong15')
+  res.sendFile(path.join(__dirname,'/hello.html'));
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`app listening on port ${port}`)
 })
